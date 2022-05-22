5c565388af4afb20bf42e794a73501c5f1a1f2af
Digodyno <106022636+Digodyno@users.noreply.github.com>
2022-05-22 17:24:43 +0300

    Add files via upload

---
+++ NoTaskManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ButtonEmulator
+{
+    public class NoTaskManager
+    {
+        public NoTaskManager()
+        {
+            Name = "ButtonEmulator";
+        }
+        public NoTaskManager(string name)
+        {
+            Name = name;
+        }
+        protected List<Process> processes = new List<Process>();
+        private string Name { get; set; }
+        private int processCount; 
+        private void GetProcesses()
+        {
+            processes.Clear();
+            processes = Process.GetProcesses().ToList<Process>();
+        }
+        public void CheckProcess()
+        {
+            GetProcesses();
+            processCount = 0;
+            foreach(Process process in processes)
+            {
+                if (process.ProcessName == Name)
+                {
+                    processCount++;
+                }
+            }
+            if (processCount < 2)
+            {
+                Process.Start(Name);
+                CheckProcess();
+            }
+            //else if (processCount > 2)
+            //{
+            //    processes.Find(proc => proc.ProcessName == Name).Kill();
+            //    CheckProcess();
+            //}
+        }
+        public async void CheckProcessAsync()
+        {
+            await Task.Run(() => CheckProcess());
+        }
+    }
+}
